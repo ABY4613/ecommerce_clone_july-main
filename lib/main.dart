4a99159dc6dfc3231025a9ece36d896e2accd894
ABY4613 <abyb4613@gmail.com>
2024-11-10 15:28:09 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: ColorConstants.SCAFFOLDBG,
       ),
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
